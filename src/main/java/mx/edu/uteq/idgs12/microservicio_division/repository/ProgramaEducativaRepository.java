0c8a9f477b83951aa747cebca4a208498668d6ad
Ricardo <rica82561@gmail.com>
2025-10-02 15:22:17 -0600

    Add ProgramaEducativo file

---
+++ src/main/java/mx/edu/uteq/idgs12/microservicio_division/repository/ProgramaEducativaRepository.java
@@ -0,0 +1,8 @@
+package mx.edu.uteq.idgs12.microservicio_division.repository;
+
+import mx.edu.uteq.idgs12.microservicio_division.entity.ProgramaEducativo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProgramaEducativaRepository extends JpaRepository<ProgramaEducativo, Long> {
+
+}
